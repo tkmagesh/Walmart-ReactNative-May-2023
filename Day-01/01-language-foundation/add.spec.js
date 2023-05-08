3a432d5f1d188ee7f5ad05915d224aebb301e540
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-08 21:00:58 +0530

    language foundation - complete

--- Day-01/01-language-foundation/add.spec.js
+++ Day-01/01-language-foundation/add.spec.js
@@ -1,7 +1,12 @@
-
-function add(/*  */){
-    /*  */
+function add(){
+    function parseArg(n){
+        if (typeof n === 'function') return parseArg(n())
+        if (Array.isArray(n)) return add.apply(this, n)
+        return isNaN(n) ? 0 : parseInt(n)
+    }
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1))
 }
+
 
 test("add(10,20) => 30", function(){
     var expectedResult = 30;
