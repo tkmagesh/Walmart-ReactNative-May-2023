91912e11cacd415f9729b05fb1c1f02412fa3aab
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-10 20:10:53 +0530

    state manager - introduction

---
+++ Day-03/01-state-manager/state-manager.js
@@ -0,0 +1,44 @@
+const StateManager = (() => {
+    let _currentState = undefined,
+        _callbacks = [],
+        _reducer = undefined,
+        __init_action = { type : '@@INIT/ACTION' }
+
+    function getState(){
+        return _currentState;
+    }
+
+    function subscribe(callbackFn){
+        _callbacks.push(callbackFn)
+    }
+
+    //private utility function
+    function _notifyChange(){
+        _callbacks.forEach(cb => cb())
+    }
+
+    function dispatch(action){
+        const newState = _reducer(_currentState, action)
+        if (newState === _currentState) return ; // no state changes
+        _currentState = newState
+        _notifyChange()
+    }
+
+    function createStore(reducer){
+        if (typeof reducer !== 'function' )
+            throw new Error('invalid reducer')
+        _reducer = reducer;
+        _currentState = _reducer(_currentState /* undefined */, __init_action)
+        const store = {
+            getState,
+            subscribe,
+            dispatch
+        }
+        return store;
+    }
+
+    return {
+        createStore
+    };
+
+})()
