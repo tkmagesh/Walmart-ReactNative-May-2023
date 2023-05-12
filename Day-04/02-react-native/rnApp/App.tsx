b3e4db32d94dab5cf2e26703b01377e9357c822e
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-12 18:08:24 +0530

    Using SectionList

--- Day-04/02-react-native/rnApp/App.tsx
+++ Day-04/02-react-native/rnApp/App.tsx
@@ -5,11 +5,12 @@ import BoxModelDemo from './components/flexbox/BoxModelDemo2';
 import BoxModelDemo1 from './components/flexbox/BoxModelDemo1';
 import ScrollViewComponent from './components/scrollViewComponent';
 import FlatListComponent from './components/flatListComponent';
+import SectionListComponent from './components/sectionListComponent';
 
 const App = () => {
   return (
 
-   <FlatListComponent/>
+   <SectionListComponent/>
   )
 }
 
---
+++ Day-04/02-react-native/rnApp/components/sectionListComponent.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { SectionList, Text, StyleSheet } from 'react-native';
+
+const sections = [
+    {
+        id: 0,
+        title: 'Basic Components',
+        data: [
+            { id: 0, text: 'View' },
+            { id: 1, text: 'Text' },
+            { id: 2, text: 'Image' },
+        ]
+    },
+    {
+        id: 1,
+        title: 'List Components',
+        data: [
+            { id: 3, text: 'ScrollView' },
+            { id: 4, text: 'ListView' },
+        ]
+    }
+]
+
+const extractKey = ({ id }) => id
+
+export default function SectionListComponent()  {
+
+    const renderItem = ({ item }) => {
+        return (
+            <Text style={styles.row}>
+                {item.text}
+            </Text>
+        )
+    }
+
+    const renderSectionHeader = ({ section }) => {
+        return (
+            <Text style={styles.header}>
+                {section.title}
+            </Text>
+        )
+    }
+
+    
+    return (
+        <SectionList
+            style={styles.container}
+            sections={sections}
+            renderItem={renderItem}
+            renderSectionHeader={renderSectionHeader}
+            keyExtractor={extractKey}
+        />
+    );
+    
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        flex: 1,
+    },
+    row: {
+        padding: 15,
+        marginBottom: 5,
+        backgroundColor: 'skyblue',
+    },
+    header: {
+        padding: 15,
+        marginBottom: 5,
+        backgroundColor: 'steelblue',
+        color: 'white',
+        fontWeight: 'bold',
+    },
+})
